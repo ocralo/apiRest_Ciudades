420cfda7f92531225a34ce39832be0d0356e7a27
RESCOBAR UAO <e_lopez1996@hotmail.com>
2019-11-21 23:56:19 -0500

    se crea la vista home

---
+++ Client/ciudades/src/views/Home/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Home(props) {
+  return (
+    <div className="">
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/docs/4.3/assets/brand/bootstrap-solid.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+          />
+          Bootstrap
+        </a>
+      </nav>
+      <div className="container mt-4">
+        <div className="row">
+            <div className="col-12">
+                
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}

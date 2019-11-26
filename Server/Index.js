954ff9202eb8aac3b4af616ba899f9a2e51ab54c
RESCOBAR UAO <e_lopez1996@hotmail.com>
2019-11-25 23:38:53 -0500

    se hacen los metodos para ver los datos

--- Server/Index.js
+++ Server/Index.js
@@ -17,7 +17,7 @@ const con = mysql.createPool({
   database: "proyecto1"
 });
 
-app.get("/", function(req, res) {
+/* app.get("/", function(req, res) {
   const sqlSelect = "SELECT * FROM Usuarios";
   con.getConnection(function(error, tempCont) {
     if (error) {
@@ -35,6 +35,63 @@ app.get("/", function(req, res) {
             res.send(rows);
           } else {
             return null;
+          }
+        }
+      });
+    }
+  });
+});
+ */
+/* Metodo para enviar los usuarios existentes */
+app.get("/users", function(req, res) {
+  const sqlSelect = "SELECT name,apellido,edad,cedula,email FROM Usuarios";
+  con.getConnection(function(error, tempCont) {
+    if (error) {
+      console.log("error coneccion DB");
+    } else {
+      console.log("Connected DB!");
+      tempCont.query(sqlSelect, function(error, rows, field) {
+        if (!!error) {
+          console.log("error en el query");
+        } else {
+          tempCont.release();
+          if (rows.length > 0) {
+            res.send(rows);
+          } else {
+            res.send(null);
+          }
+        }
+      });
+    }
+  });
+});
+
+/* Metodo para enviar los datos del usuario seleccionado */
+app.post("/users/data", function(req, res) {
+  const sqlSelect = `SELECT Sensores.data, 
+  Sensores.dateSensing, Sensores.nameSensor, 
+  Dispositivos.nameDispositivo, Dispositivos.zone, 
+  Usuarios.name, Usuarios.apellido, Usuarios.edad 
+  FROM Sensores, Dis_Sensor, Dispositivos, 
+  Usuarios WHERE Sensores.idSensor=Dis_Sensor.fkSensor 
+  AND Dis_Sensor.fkDispositivo=Dispositivos.idDispositivo 
+  AND Dispositivos.fkUser=Usuarios.idUsuario 
+  AND Usuarios.name='${req.body.name}'`;
+  console.log(req.body.name);
+  con.getConnection(function(error, tempCont) {
+    if (error) {
+      console.log("error coneccion DB");
+    } else {
+      console.log("Connected DB!");
+      tempCont.query(sqlSelect, function(error, rows, field) {
+        if (!!error) {
+          console.log("error en el query");
+        } else {
+          tempCont.release();
+          if (rows.length > 0) {
+            res.send(rows);
+          } else {
+            res.send(null);
           }
         }
       });
@@ -69,7 +126,7 @@ app.post("/login", function(req, res) {
   });
 });
 
-app.post("/", function(req, res) {
+/* app.post("/", function(req, res) {
   console.log(req.body.temp);
   res.send(req.body);
   const sqlSelect = `INSERT INTO tablita (variable,valor) VALUES ('${req.body.sen}','${req.body.val}')`;
@@ -87,7 +144,7 @@ app.post("/", function(req, res) {
       });
     }
   });
-});
+}); */
 
 app.listen(8080, function() {
   console.log("Example app listening on port 8080!");
